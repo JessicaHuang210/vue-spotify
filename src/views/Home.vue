cd081a3662bf6b591b77d59293f0c90ace3268a9
黃婉柔 <>
2019-08-28 22:43:04 +0800

    bb

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,22 +6,34 @@
         <h3 class="title-border-b mt-2">{{tabs[0].name}}</h3>
         <div class="grid">
           <div class="grid__item">
-            <img class src="@/assets/h01.jpeg" />
+            <img class src="https://t.scdn.co/images/fd2cdde10ea04ccfb10c902274be8815.jpeg" />
           </div>
           <div class="grid__item">
-            <img class src="@/assets/h02.jpg" />
+            <img
+              class
+              src="https://t.scdn.co/media/derived/pop-274x274_447148649685019f5e2a03a39e78ba52_0_0_274_274.jpg"
+            />
           </div>
           <div class="grid__item">
-            <img class src="@/assets/h03.jpg" />
+            <img
+              class
+              src="https://t.scdn.co/media/original/mood-274x274_976986a31ac8c49794cbdc7246fd5ad7_274x274.jpg"
+            />
           </div>
           <div class="grid__item">
-            <img class src="@/assets/h04.jpg" />
+            <img
+              class
+              src="https://t.scdn.co/media/derived/chill-274x274_4c46374f007813dd10b37e8d8fd35b4b_0_0_274_274.jpg"
+            />
           </div>
           <div class="grid__item">
-            <img class src="@/assets/h05.jpeg" />
+            <img class src="https://t.scdn.co/images/69c728f3bd9643a5ab0f4ef5a79919f1.jpeg" />
           </div>
           <div class="grid__item">
-            <img class src="@/assets/h06.jpg" />
+            <img
+              class
+              src="https://t.scdn.co/media/original/genre-images-square-274x274_5e50d72b846a198fcd2ca9b3aef5f0c8_274x274.jpg"
+            />
           </div>
         </div>
       </div>
