a66400b9d5f691c4358bcfa92318d434e8847349
黃婉柔 <>
2019-08-28 22:32:51 +0800

    grid 3 colums

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,37 @@
       <div slot="0">
         <h3 class="title-border-b mt-2">{{tabs[0].name}}</h3>
         <div class="grid">
-          <div class="grid__item"></div>
+          <div class="grid__item">
+            <img class src="@/assets/h01.jpeg" />
+          </div>
+          <div class="grid__item">
+            <img class src="@/assets/h02.jpg" />
+          </div>
+          <div class="grid__item">
+            <img class src="@/assets/h03.jpg" />
+          </div>
+          <div class="grid__item">
+            <img class src="@/assets/h04.jpg" />
+          </div>
+          <div class="grid__item">
+            <img class src="@/assets/h05.jpeg" />
+          </div>
+          <div class="grid__item">
+            <img class src="@/assets/h06.jpg" />
+          </div>
         </div>
+      </div>
+      <div slot="1">
+        <h3 class="title-border-b mt-2">精選排行榜</h3>
+      </div>
+      <div slot="2">
+        <h3 class="title-border-b mt-2">最棒的最新發行音樂</h3>
+      </div>
+      <div slot="3">
+        <h3 class="title-border-b mt-2">為您打造的播放清單</h3>
+      </div>
+      <div slot="4">
+        <h3 class="title-border-b mt-2">{{tabs[4].name}}</h3>
       </div>
     </Tabs>
   </div>
