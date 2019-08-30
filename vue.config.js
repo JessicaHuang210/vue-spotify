5b0423ac00350ac9d17887b3840dad116f0a2f92
黃婉柔 <>
2019-08-30 23:18:48 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     port: 6800, // 配置端口
     open: true
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-spotify" : "/",
+  publicPath: "./",
   configureWebpack: {
     resolve: {
       alias: {
