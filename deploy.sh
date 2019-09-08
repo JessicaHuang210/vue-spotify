1f1d36dac3996568160105206603d2f9e2144245
黃婉柔 <>
2019-09-08 16:05:22 +0800

    update分之部署

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/JessicaHuang210/vue-spotify.git master
+git push -f https://github.com/JessicaHuang210/vue-spotify.git master:gh-pages
 cd -
